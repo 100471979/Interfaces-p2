b86d956ee4880a96ada05be19de12b91896c27ae
100471979 <100471979@alumnos.uc3m.es>
2023-10-15 03:11:05 +0200

    registro-version-1

---
+++ script/script.js
@@ -0,0 +1,45 @@
+function limpiarCampos() {
+    document.getElementById("form").reset();
+}
+
+function FormatoDNI(dni){
+
+    let digito = dni.slice(0, 8);
+    let letra = dni.charAt(8).toUpperCase();
+    let letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    let resto = digito % 23;
+    let macth = letras.charAt(resto);
+    return letra == macth;
+
+}
+function Verificar(event) {
+    event.preventDefault();
+    let correo = document.getElementById("correo").value;
+    let nombre = document.getElementById("nombre").value;
+    let dni = document.getElementById("DNI").value;
+    let telefono = document.getElementById("telefono").value;
+    if (dni == ""){
+        document.getElementById("demo").innerHTML = "Ingrese su DNI";
+    }
+    else if(dni.length != 9 || FormatoDNI(dni) == false){
+        document.getElementById("demo").innerHTML = "Formato incorrecto del DNI"
+    }
+    else if(nombre == ""){
+        document.getElementById("demo").innerHTML = "Ingrese su nombre"
+    }
+    else if(telefono == ""){
+        document.getElementById("demo").innerHTML = "Ingrese su número de teléfono"
+    }
+    else if(isNaN(telefono) || telefono.length != 9){
+        document.getElementById("demo").innerHTML = "El campo teléfono debe ser un número de 9 cifras";
+
+    }
+    else if( correo == ""){
+        document.getElementById("demo").innerHTML = "Ingrese su correo"
+    }  
+    else{     
+        document.getElementById("demo").innerHTML = "";
+        document.getElementById("form").submit();
+    }
+    
+}
