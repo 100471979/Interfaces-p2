f770fb080b6b43ed9f2fb056eeb16bf7883e07d3
100471979 <100471979@alumnos.uc3m.es>
2023-10-21 01:20:47 +0200

    js para el carrito, falta calcular precio total

--- script/script.js
+++ script/script.js
@@ -54,5 +54,65 @@ function Verificar(event) {
     
 }
 
-//carrito
+//ocultar pasos del bread-crumbs
+var revisar = document.getElementById("revisar");
+var menu = document.getElementById("menu");
+var enlaceRevisar = document.getElementById("enlace-revisar");
+var enlaceMenu = document.getElementById("enlace-menu");
 
+enlaceRevisar.addEventListener("click", function(){
+    menu.style.display = "none";
+    revisar.style.display = "block";
+
+});
+
+enlaceMenu.addEventListener("click", function(){
+    menu.style.display = "block";
+    revisar.style.display = "none";
+
+});
+
+//botones añadir y quitar productos
+var totalProductos = document.getElementById("total-productos");
+var totalProductos2 = document.getElementById("total-productos2");
+
+
+function añadir_quitar(producto, cantidad){
+    var carrito1 = parseInt(totalProductos.textContent);
+    var carrito2 = parseInt(totalProductos2.textContent);
+
+    if(carrito1 + cantidad >= 0 || carrito1 + cantidad >= 0){
+        carrito1 += cantidad;
+        carrito2 += cantidad;
+    }
+    
+    totalProductos.textContent = carrito1;
+    totalProductos2.textContent = carrito2;
+
+    var cantidad_plato = document.getElementById("cantidad_plato" + producto);
+    var cantidad_actual_plato = parseInt(cantidad_plato.textContent);
+    
+    if(cantidad_actual_plato + cantidad >= 0){
+    
+        cantidad_actual_plato += cantidad;
+        cantidad_plato.textContent = cantidad_actual_plato;
+    }
+    
+}  
+
+var añadir = document.querySelectorAll("button[id^='añadido']");
+var quitar = document.querySelectorAll("button[id^='quitar']");
+añadir.forEach(function(boton){
+    boton.addEventListener("click", function(){
+        var producto = boton.id.replace("añadido", "");
+        añadir_quitar(producto, 1);
+    });
+
+});
+
+quitar.forEach(function(boton){
+    boton.addEventListener("click", function(){
+        var producto = boton.id.replace("quitar", "");
+        añadir_quitar(producto, -1);
+    });
+});
