e5e28f14a2eeffe150459747b188d2e9aa4eaeb6
100471979 <100471979@alumnos.uc3m.es>
2023-10-23 17:21:54 +0200

    menu hamburguesa hechisimo

--- script/script.js
+++ script/script.js
@@ -171,3 +171,15 @@ enlaceCompletado.addEventListener('click', function(event){
         event.preventDefault();
     }
 });
+
+var nav = document.querySelector("#nav1");
+var abrir = document.querySelector("#abrir");
+var cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
